2633b8ec4691e58352f1406473a22a4cc351f727
Jimin Yeon <gmini.y@gmail.com>
2020-03-04 01:13:52 +0900

    feat: init get delivery track router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json();
+router.get('/delivery-track', function(req, res, next) {
+  const { deliveryCompanyCode, invoiceNumber } = req.params;
+  res.json({ deliveryCompanyCode, invoiceNumber });
 });
 
 module.exports = router;
