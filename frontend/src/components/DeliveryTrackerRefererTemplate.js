b399c0095e20922fa7bd15d093816bae45aca41b
Jimin Yeon <gmini.y@gmail.com>
2020-03-03 19:55:19 +0900

    feat: add buttons to DeliverTrackerRefererTemplate
    - refer button
    - init button

--- frontend/src/components/DeliveryTrackerRefererTemplate.js
+++ frontend/src/components/DeliveryTrackerRefererTemplate.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import DeliveryTrackerHeader from './DeliveryTrackerHeader';
 import DeliveryTrackerInputForm from './DeliveryTrackerInputForm';
+import DeliveryTrackerButton from './DeliveryTrackerButton';
 
+const DeliveryTrackerButtonWithMargin = styled(DeliveryTrackerButton)`
+  margin-left: 3%;
+  margin-top: 2.5%;
+`
 const DeliveryTrackerInputTemplate = () => {
   return (
     <div>
-      <DeliveryTrackerHeader title="검색 정보" />
+      <DeliveryTrackerHeader title="조회 정보" />
       <DeliveryTrackerInputForm name="deliveryCompany" label="택배 회사" />
       <DeliveryTrackerInputForm name="invoiceNumber" label="송장 번호" />
+      <DeliveryTrackerButtonWithMargin>조회하기</DeliveryTrackerButtonWithMargin>
+      <DeliveryTrackerButtonWithMargin>초기화</DeliveryTrackerButtonWithMargin>
     </div>
   );
 };
