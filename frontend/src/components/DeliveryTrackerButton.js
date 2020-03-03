0902621978b3436fea76c7f146bb83eb9c674c68
Jimin Yeon <gmini.y@gmail.com>
2020-03-03 19:28:11 +0900

    fix: fix padding typo

--- frontend/src/components/DeliveryTrackerButton.js
+++ frontend/src/components/DeliveryTrackerButton.js
@@ -4,10 +4,10 @@ import palette from '../lib/styles/palette';
 
 const StyledButton = styled.button`
   border: none;
-  border-radius: 4px;
+  border-radius: 2px;
   font-size: 1rem;
   font-weight: bold;
-  padding: 0.25rem, 1rem;
+  padding: 0.25rem 1rem;
   color: white;
   outline: none;
   cursor: pointer;
