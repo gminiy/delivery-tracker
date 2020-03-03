56e8e0db42ae46be3f5388f877c1a658d6be01eb
Jimin Yeon <gmini.y@gmail.com>
2020-03-03 15:30:17 +0900

    feat: style DeliveryTrackerTemplate

--- frontend/src/components/DeliveryTrackerTemplate.js
+++ frontend/src/components/DeliveryTrackerTemplate.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './DeliveryTrackerTemplate.scss';
 
 const DeliveryTrackerTemplate = ({ children }) => {
   return (
     <div className="DeliveryTrackerTemplate">
-      <div className="app-title">배송 조회</div>
+      <div className="title">월간 가슴 배송 조회</div>
       <div className="content">{children}</div>
     </div>
   );
