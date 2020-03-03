3dc13741a32dd576f97f19f9d98db9779c7a90b0
Jimin Yeon <gmini.y@gmail.com>
2020-03-03 19:13:40 +0900

    feat: create DeliverTrackerInputForm
    - DeliverTrackerInputForm will be used commonly

---
+++ frontend/src/components/DeliveryTrackerInputForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../lib/styles/palette';
+
+const DeliveryTrackerInputFormBlock = styled.div`
+  margin-top: 1%;
+  margin-left: 3%;
+
+  .inputLabel {
+    font-weight: bold;
+    display: inline-block;
+    width: 30%;
+    color: ${palette.gray[7]};
+  }
+`;
+
+const StyledInput = styled.input`
+  font-size: 1rem;
+  margin-left: 1%;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[8]};
+  padding: 0.3rem;
+  width: 20rem;
+`;
+
+const DeliveryTrackerInput = ({ label, name }) => {
+  return (
+    <DeliveryTrackerInputFormBlock>
+      <div className="inputLabel">{label}</div>
+      <StyledInput name={name} placeholder={label} />
+    </DeliveryTrackerInputFormBlock>
+  );
+};
+
+export default DeliveryTrackerInput;
--- frontend/src/components/DeliveryTrackerInputTemplate.js
+++ frontend/src/components/DeliveryTrackerInputTemplate.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import DeliveryTrackerHeader from './DeliveryTrackerHeader';
+import DeliveryTrackerInputForm from './DeliveryTrackerInputForm';
 
 const DeliveryTrackerInputTemplate = () => {
   return (
-    <DeliveryTrackerHeader title="검색 정보" />
+    <div>
+      <DeliveryTrackerHeader title="검색 정보" />
+      <DeliveryTrackerInputForm name="deliveryCompany" label="택배 회사" />
+      <DeliveryTrackerInputForm name="invoiceNumber" label="송장 번호" />
+    </div>
   );
 };
 
