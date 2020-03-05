0285402343621000818c541ba9ad2b433718c432
Jimin Yeon <gmini.y@gmail.com>
2020-03-05 17:33:38 +0900

    fix: api url

--- frontend/src/lib/api/referer.js
+++ frontend/src/lib/api/referer.js
@@ -2,5 +2,5 @@ import client from './client';
 
 export const referDeliveryTracking = ({ deliveryCompany, invoiceNumber }) =>
   client.get(
-    `/delivery-tracking?deliveryCompanyCode=${deliveryCompany}&invoiceNumber=${invoiceNumber}`,
+    `/api/delivery-tracking?deliveryCompanyCode=${deliveryCompany}&invoiceNumber=${invoiceNumber}`,
   );
